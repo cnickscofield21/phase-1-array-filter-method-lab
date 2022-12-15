7a6a8a3dd6c51ba6916bf6eafd5de01933db6598
Nick Scofield <c.nick.scofield@gmail.com>
2022-12-14 23:08:48 -0700

    commit tree a256b3f94b92c5482ecb0ea09d685cb367998217

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers, pattern) {
+    const regEx = new RegExp(pattern, "i");
+    const matches = drivers.filter(element => regEx.test(element));
+
+    return matches;
+}
+
+function fuzzyMatch(drivers, pattern) {
+    const regEx = new RegExp(`^${pattern}`, "i");
+    const matches = drivers.filter(element => regEx.test(element));
+
+    return matches;
+}
+
+function matchName(drivers, pattern) {
+    const regEx = new RegExp(pattern, "i");
+    const matches = drivers.filter(element => regEx.test(element.name));
+
+    return matches;
+}
